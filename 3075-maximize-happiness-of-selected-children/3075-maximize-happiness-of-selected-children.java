6fac350aa0e60b1b2d929b217bc66fc656da815f
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2025-12-25 14:24:46 +0530

    Time: 42 ms (70.97%), Space: 109.6 MB (17.74%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);   // ascending
+
+        long sum = 0;
+        int n = happiness.length;
+
+        // pick from largest values
+        for (int i = 0; i < k; i++) {
+            int value = happiness[n - 1 - i] - i;
+            if (value > 0) {
+                sum += value;
+            }
+        }
+
+        return sum;
+    }
+}
