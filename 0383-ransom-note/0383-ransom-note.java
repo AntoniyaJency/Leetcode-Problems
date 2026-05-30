4c60f961b925ab612546dca55ea2caddd9b6aa5e
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-05-30 21:05:05 +0530

    Time: 1 ms (99.91%), Space: 46.1 MB (89.14%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        // count characters in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+
+        // use characters for ransomNote
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            count[ch - 'a']--;
+
+            if (count[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
