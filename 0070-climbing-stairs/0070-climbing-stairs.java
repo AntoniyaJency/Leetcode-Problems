eb85df7c9f6941609a5387a093098207aa919793
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-06-01 13:03:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2) return n;
-        int a=1,b=2,c=0;
-        for(int i=3;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-
-        }
-        return c;
+        int[] dp=new int[n+1];
+        return solve(n,dp); 
     }
+
+    public int solve(int n,int dp[]){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(dp[n]!=0) return dp[n];
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
+
 }
