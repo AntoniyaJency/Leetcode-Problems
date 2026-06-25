dad44849e9b7ab319034b2fbafb2cd1c808fd5d7
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-06-25 11:07:41 +0530

    Time: 2 ms (41.81%), Space: 94.5 MB (50.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int profit=0;
-        int diff=prices[0];
+        int min=prices[0];
         
         for(int i=1;i<prices.length;i++){
-            if(prices[i]<diff) diff=prices[i];
+            if(prices[i]<min) min=prices[i];
             else{
-                profit=Math.max(prices[i]-diff,profit);
+                profit=Math.max(prices[i]-min,profit);
 
             }
         }
-
         return profit;
     }
 }
