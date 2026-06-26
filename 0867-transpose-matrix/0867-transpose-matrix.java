69956aa9cf8384a22cfd1ab12bfc76130fa4c0f9
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-06-26 08:39:07 +0530

    Time: 0 ms (100%), Space: 46.6 MB (83.97%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -1,12 +1,15 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        int[][] res=new int[matrix[0].length][matrix.length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                res[j][i]=matrix[i][j];
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int arr[][]=new int[col][row];
+        for(int i=0;i<col;i++)
+        {
+            for(int j=0;j<row;j++)
+            {
+            arr[i][j]=matrix[j][i];
             }
         }
-
-        return res;
+        return arr;
     }
 }
