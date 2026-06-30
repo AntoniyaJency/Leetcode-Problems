19117249d04cbc0d50c06aad3f8339b555d0f9be
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-06-30 11:54:43 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,8 +5,7 @@ class Solution {
     }
 
     public int solve(int n,int dp[]){
-        if(n==1) return 1;
-        if(n==2) return 2;
+        if(n==1 || n==2) return n;
         if(dp[n]!=0) return dp[n];
         dp[n]=solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
