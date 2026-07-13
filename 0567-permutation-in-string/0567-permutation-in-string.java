0da34701a5aa00d23e8490316798656d2fd54d88
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-07-13 22:47:25 +0530

    Time: 7 ms (69.1%), Space: 44.1 MB (48.73%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        
+        int[] count = new int[26];
+        for (int i = 0; i < s1.length(); i++) 
+            count[s1.charAt(i) - 'a']++;
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+            count[s2.charAt(right) - 'a']--;
+
+            while (count[s2.charAt(right) - 'a'] < 0) {
+                count[s2.charAt(left) - 'a']++;
+                left++;
+            }
+
+            if (right - left + 1 == s1.length())  return true;
+        }
+        return false;
+    }
+}
