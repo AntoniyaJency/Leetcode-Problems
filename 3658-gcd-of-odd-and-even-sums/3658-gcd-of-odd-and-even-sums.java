9802a44da46f119b2485241c9980cf64a78b0b5c
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-07-15 19:05:12 +0530

    Time: 1 ms (83.11%), Space: 42.8 MB (20.94%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+       /* int oddSum=(n/2)*(2+2*(n-1));
+        int evenSum=(n/2)*(4+2*(n-1));
+        return gcd(oddSum,evenSum);*/
+    }
+
+   /* public int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }*/
+}
