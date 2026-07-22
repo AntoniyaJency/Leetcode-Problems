4853ea19c25027ba779cdb25dc0248f253d68894
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-07-22 20:04:14 +0530

    Time: 2 ms (88.98%), Space: 47.5 MB (96.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,14 +4,8 @@ class Solution {
         int maxReach = 0;
 
         for(int i = 0; i < nums.length; i++) {
-
-            // if current index is not reachable
             if(i > maxReach) return false;
-
-            // update farthest reachable position
             maxReach = Math.max(maxReach, i + nums[i]);
-
-            // if we already reached end
             if(maxReach >= nums.length - 1) return true;
         }
 
