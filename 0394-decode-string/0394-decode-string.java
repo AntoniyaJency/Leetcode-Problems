10744e1b86f31dd4cc7e32dcd38eef7f022fc31f
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-07-29 20:23:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.4%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,41 +1,30 @@
 class Solution {
+    int i = 0;
     public String decodeString(String s) {
-        Stack<Integer> numStack = new Stack<>();
-        Stack<String> strStack = new Stack<>();
-        
-        String curr = "";
+        i = 0;
+        return decode(s);
+    }
+    private String decode(String s) {
+        StringBuilder res = new StringBuilder();
         int num = 0;
-        
-        for (char ch : s.toCharArray()) {
-            
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            }
-            
-            else if (ch == '[') {
-                numStack.push(num);
-                strStack.push(curr);
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+                i++;
+            } else if (c == '[') {
+                i++;
+                String inner = decode(s);
+                for (int k = 0; k < num; k++) res.append(inner);
                 num = 0;
-                curr = "";
-            }
-            
-            else if (ch == ']') {
-                int k = numStack.pop();
-                String prev = strStack.pop();
-                
-                String temp = "";
-                for (int i = 0; i < k; i++) {
-                    temp += curr;
-                }
-                
-                curr = prev + temp;
-            }
-            
-            else {
-                curr += ch;
+            } else if (c == ']') {
+                i++;
+                return res.toString();
+            } else {
+                res.append(c);
+                i++;
             }
         }
-        
-        return curr;
+        return res.toString();
     }
 }
