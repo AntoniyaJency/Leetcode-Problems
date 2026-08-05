780c27967cc3a16969d8b467eda4aa77e38e2b91
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-05 21:26:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (84.75%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] sn=s.split("\\s+");
-        String res=sn[sn.length-1];
-        return res.length();
+        s = s.trim();
+        
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+        
+        return length;
     }
 }
