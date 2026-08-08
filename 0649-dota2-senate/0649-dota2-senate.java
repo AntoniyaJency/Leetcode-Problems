acfe89617f130f397c2b5f44709e5e7433be44a8
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-08 15:42:23 +0530

    Time: 6 ms (97.24%), Space: 46.1 MB (92.45%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,24 +1,47 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        PriorityQueue<Integer> rq=new PriorityQueue<>();
-        PriorityQueue<Integer> dq=new PriorityQueue<>();
-
-        int n=senate.length();
-
-        for(int i=0;i<n;i++){
-            char c=senate.charAt(i);
-            if(c=='R') rq.add(i);
-            else dq.add(i);
+        Queue<Character> q = new LinkedList<>();
+        int br = 0, bd = 0;
+        int r = 0, d = 0;
+        for (char c : senate.toCharArray()) {
+            if (c == 'R') {
+                if (br > 0) {
+                    br--;
+                    continue;
+                }
+                q.offer('R');
+                bd++;
+                r++;
+            } else {
+                if (bd > 0) {
+                    bd--;
+                    continue;
+                }
+                q.offer('D');
+                br++;
+                d++;
+            }
         }
-
-        while(!rq.isEmpty() && !dq.isEmpty()){
-            int r=rq.poll();
-            int d=dq.poll();
-
-            if(r<d) rq.add(r+n);
-            else dq.add(d+n);
+        while (r > 0 && d > 0) {
+            char c = q.poll();
+            if (c == 'R') {
+                if (br > 0) {
+                    br--;
+                    r--;
+                    continue;
+                }
+                q.offer('R');
+                bd++;
+            } else {
+                if (bd > 0) {
+                    bd--;
+                    d--;
+                    continue;
+                }
+                q.offer('D');
+                br++;
+            }
         }
-
-        return rq.isEmpty()? "Dire":"Radiant";
+        return r > 0 ? "Radiant" : "Dire";
     }
 }
