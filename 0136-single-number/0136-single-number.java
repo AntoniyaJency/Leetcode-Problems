5e9312067cf2c2b4428313a0163071b779f11943
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-09 20:47:57 +0530

    Time: 1 ms (99.95%), Space: 47 MB (28.44%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int res = 0;
-        for (int n : nums) res ^= n;
-        return res;
+
+        for (int n : nums) {
+            res ^= n;
+        }
+
+        return res;        
     }
 }
