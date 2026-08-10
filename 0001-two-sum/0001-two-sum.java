eaa8052d60588097045d0e41d46dc7750b5c563a
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-10 10:50:52 +0530

    Time: 38 ms (42.06%), Space: 47.1 MB (59.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,48 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-       
-        /*int left=0;
-        int right=nums.length-1;
-        while(left<right){
-            int sum=nums[left]+nums[right];
-            if(sum==target) return new int[]{left,right};
-            else if(sum>target) right--;
-            else left++;
-        }
-
-      return new int[]{-1,-1};*/
-
-      int n = nums.length;
-
-      
-        int[][] arr = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = nums[i]; 
-            arr[i][1] = i;        
-        }
-
-        
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-
-        int left = 0;
-        int right = n - 1;
-
-        while (left < right) {
-            int sum = arr[left][0] + arr[right][0];
-
-            if (sum == target) {
-                return new int[]{arr[left][1], arr[right][1]};
-            } 
-            else if (sum > target) {
-                right--;
-            } 
-            else {
-                left++;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target) return new int[]{i,j};
             }
         }
 
-        return new int[]{-1, -1};
+        return new int[]{-1,-1};
     }
 }
