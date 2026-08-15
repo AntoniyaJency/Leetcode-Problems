a9e6c19d437c0fbb9a100c24a584742179f35b39
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-15 23:13:56 +0530

    Time: 3 ms (86.11%), Space: 42.7 MB (99.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,15 +3,12 @@ import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             } 
-           
             else {
                 if (stack.isEmpty()) return false;
 
