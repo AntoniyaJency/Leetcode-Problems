282089dfb75d4d008769cb66f51759a29912070d
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-15 23:08:27 +0530

    Time: 83 ms (19.95%), Space: 102 MB (86.18%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+
+        int n = nums1.length;
+
+        int[][] pairs = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums1[i];
+            pairs[i][1] = nums2[i];
+        }
+
+        Arrays.sort(pairs, (a, b) -> b[1] - a[1]);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        long sum = 0;
+        long answer = 0;
+
+        for (int[] pair : pairs) {
+
+            int num1 = pair[0];
+            int num2 = pair[1];
+
+            minHeap.offer(num1);
+            sum += num1;
+
+            if (minHeap.size() > k) {
+                sum -= minHeap.poll();
+            }
+
+            if (minHeap.size() == k) {
+                answer = Math.max(answer, sum * num2);
+            }
+        }
+
+        return answer;
+    }
+}
