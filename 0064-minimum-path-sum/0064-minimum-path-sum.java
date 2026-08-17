59e04783006d9a02577226f776e9c17299db8e1a
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-17 10:25:09 +0530

    Time: 3 ms (73.19%), Space: 50.7 MB (10.3%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,21 +1,16 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int row=grid.length;
-        int col=grid[0].length;
-        for(int i=1;i<col;i++){
-            grid[0][i]+=grid[0][i-1];
-        }
-        
-        for(int j=1;j<row;j++){
-            grid[j][0]+=grid[j-1][0];
-        }
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=1;i<n;i++) grid[0][i]+=grid[0][i-1];
+        for(int i=1;i<m;i++) grid[i][0]+=grid[i-1][0];
 
-        for(int i=1;i<row;i++){
-            for(int j=1;j<col;j++){
-                grid[i][j]+=Math.min(grid[i][j-1],grid[i-1][j]);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
             }
         }
-        return grid[row-1][col-1];
 
+        return grid[m-1][n-1];
     }
 }
