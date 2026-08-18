2f7b35afb8a9e94e032aa8ee9992650660da1529
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-18 12:52:51 +0530

    Time: 2 ms (72.63%), Space: 45.4 MB (48.07%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,23 +1,53 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
 
-        for(int i = 0; i <= nums.length - k; i++) {
-            HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
 
-            for(int j = i; j < i + k; j++)
-                set.add(nums[j]);
-
-            for(int x : set)
-                map.put(x, map.getOrDefault(x, 0) + 1);
+        for (int i = 0; i < n; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
         }
 
-        int ans = -1;
+        if (k == nums.length) {
+            int maxValue = Integer.MIN_VALUE;
 
-        for(int x : map.keySet())
-            if(map.get(x) == 1)
-                ans = Math.max(ans, x);
+            for (int i = 0; i < n; i++) {
+                maxValue = Math.max(maxValue, nums[i]);
+            }
 
-        return ans;
+            return maxValue;
+        }
+
+        if (k == 1) {
+            int maxValue = -1;
+
+            for (int i = 0; i < n; i++) {
+                if (mp.get(nums[i]) == 1 && nums[i] > maxValue) {
+                    maxValue = nums[i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        n = n - 1;
+
+        if (nums[0] == nums[n]) {
+            return -1;
+        }
+
+        if (mp.get(nums[0]) == 1 && mp.get(nums[n]) == 1) {
+            return Math.max(nums[0], nums[n]);
+        }
+
+        if (mp.get(nums[0]) == 1 && mp.get(nums[n]) > 1) {
+            return nums[0];
+        }
+
+        if (mp.get(nums[n]) == 1 && mp.get(nums[0]) > 1) {
+            return nums[n];
+        }
+
+        return -1;
     }
 }
