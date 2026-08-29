e809260369b78e29c1b381e8263503d181d4927d
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-08-29 17:56:35 +0530

    Time: 0 ms (100%), Space: 42.7 MB (96.56%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -18,9 +18,15 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         if(root==null) return list;
 
-        list.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        Stack<TreeNode> s=new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode node=s.pop();
+            list.add(node.val);
+            if(node.right!=null) s.push(node.right);
+            if(node.left!=null) s.push(node.left);
+        }
+
         return list;
     }
 }
