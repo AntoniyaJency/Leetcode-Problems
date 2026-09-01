549be0d47dd6ba76f55fb0dedad5c7d9fb7e7c67
ANTONIYA JENCY <antoniyajency.27csa@licet.ac.in>
2026-09-01 20:18:57 +0530

    Time: 295 ms (83.42%), Space: 0B (100%) - LeetHub

---
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -0,0 +1,19 @@
+WITH Ranked AS (
+    SELECT
+        student_id,
+        subject,
+        FIRST_VALUE(score) OVER(
+            PARTITION BY student_id, subject
+            ORDER BY exam_date
+        ) AS first_score,
+
+        FIRST_VALUE(score) OVER(
+            PARTITION BY student_id, subject
+            ORDER BY exam_date DESC
+        ) AS latest_score
+
+    FROM Scores
+)
+
+select distinct * from Ranked
+where first_score<latest_score
